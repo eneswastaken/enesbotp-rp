b6e3ae234709ff007019257ddb41dafa654b8e2f
Glitch (band-toothsome-broccoli) <none>
2022-10-24 22:16:15 +0000

    👩‍👩‍👦📹 Checkpoint ./commands/yardım.js:49573560/76 ./events/ready.js:49573560/20

--- events/ready.js
+++ events/ready.js
@@ -26,5 +26,5 @@ module.exports = async (client) => {
   }
 
     console.log(`${client.user.tag} Aktif!`);
-    client.user.setActivity(`/botlist-ayarla | Botlist Botu`)
+    client.user.setActivity(`/yardım | Botlist Botu`)
 };
