09e091e4ee7975421cb3412b1caf759f919f978f
Glitch (band-toothsome-broccoli) <none>
2022-10-24 21:28:17 +0000

    🏦🤘 Checkpoint ./commands/yardım.js:49573560/1166 ./commands/ban.js:49573560/5475 ./commands/kick.js:49573560/868

--- commands/yardım.js
+++ commands/yardım.js
@@ -11,23 +11,19 @@ module.exports = {
     const embed = new EmbedBuilder()
     .setTitle("Mercy Botlist - Yardım!")
     .setThumbnail("https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png")
-    .setDescription("**・Moderasyon Sistemi ↷**\n > Moderasyon Sistemi hakkında bilgi alabilirsiniz.⠀⠀⠀⠀⠀\n\n**・Kayıt Sistemi ↷**\n> Kayıt Sistemi hakkında bilgi alabilirsiniz.\n\n**・Kullanıcı Sistemi ↷**\n> Kullanıcı Sistemi hakkında bilgi alabilirsiniz.")
+    .setDescription("**<:r_yesil:1034210144260718633> /botlist-ayarla** - Butonlu Botlist Ayarlama Komutu Kolay Ve Basit\n**<:r_yesil:1034210144260718633> /ban** - Sunucudan Kullanıcı Banlama")
     .setColor("000000")
     .setFooter({ text: "Mercy Botlist" })
     const row = new Discord.ActionRowBuilder()
     .addComponents(
 new Discord.ButtonBuilder()
 .setLabel("Botu Ekle")
-                    .setStyle(Discord.ButtonStyle.Link)
-                    .setURL("https://discord.com/oauth2/authorize?client_id=1034130573796593714&scope=bot&permissions="),
+.setStyle(Discord.ButtonStyle.Link)
+.setURL("https://discord.com/api/oauth2/authorize?client_id=1034130573796593714&permissions=8&scope=bot"),
 new Discord.ButtonBuilder()
-.setLabel("Kayıt")
-.setStyle(Discord.ButtonStyle.Success)
-.setCustomId("kayıt"),
-new Discord.ButtonBuilder()
-.setLabel("Kullanıcı")
-.setStyle(Discord.ButtonStyle.Primary)
-.setCustomId("kullanıcı"))
+.setLabel("Destek Sunucusu")
+.setStyle(Discord.ButtonStyle.Link)
+.setURL("https://discord.gg/ZyQWrgKg66"),)
 interaction.reply({embeds: [embed], components: [row], ephemeral: true})
   }
 
