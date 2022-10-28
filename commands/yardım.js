992e6d1a0fd2976a2f9488e7a9e4b7c8f3444a0c
Glitch (band-toothsome-broccoli) <none>
2022-10-28 08:33:13 +0000

    👕🚆 Checkpoint ./commands/yardım.js:49573560/24

--- commands/yardım.js
+++ commands/yardım.js
@@ -11,7 +11,7 @@ module.exports = {
     const embed = new EmbedBuilder()
     .setTitle("Mercy Botlist - Yardım!")
     .setThumbnail("https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png")
-    .setDescription("\n**__Kısa Bilgi__**\n\n**<:r_mod:1033846559600807996> prefix:** */*\n<:r_tac:1034208114091769866> Sahib: <@847596022020309013>\n<:r_dunya:1034209747336957992> Ping: \`15\`\n\n**__Botlist Komutlar__**\n\n<:r_bot:1033846888681722007> \`Botlist\` Komutları İçin Tıkla\n<:r_mod:1033846559600807996> \`Moderasyon\` Komutları İçin Tıkla\n<:r_kullanici:1034207931178168412> \`Kullanıcı\` Komutları İçin Tıkla")
+    .setDescription("\n**__Kısa Bilgi__**\n\n**<:r_mod:1033846559600807996> prefix:** */*\n<:r_tac:1034208114091769866> Sahib: <@847596022020309013>\n<:r_dunya:1034209747336957992> Ping: \`15\`\n\n**__Komutlar__**\n\n<:r_bot:1033846888681722007> \`Botlist\` Komutları İçin Tıkla\n<:r_mod:1033846559600807996> \`Moderasyon\` Komutları İçin Tıkla\n<:r_kullanici:1034207931178168412> \`Kullanıcı\` Komutları İçin Tıkla")
     .setColor("000000")
     .setFooter({ text: "Mercy Botlist" })
     const row = new Discord.ActionRowBuilder()
