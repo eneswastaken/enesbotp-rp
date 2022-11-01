e3bb4820a6a23fe2b5104aed0686a92ce968ec07
Glitch (band-toothsome-broccoli) <none>
2022-11-01 17:52:11 +0000

    🐑🌝 Checkpoint ./commands/bot-ekle-şart.js:50413403/5791

--- commands/bot-ekle-şart.js
+++ commands/bot-ekle-şart.js
@@ -1,21 +1,25 @@
 const { Client, EmbedBuilder } = require("discord.js");
-const Discord = require("discord.js")
+const moment = require("moment");
+  require("moment-duration-format");
+  const os = require("os");
 module.exports = {
-  name: "yardım",
-  description: "Botun yardım menüsüne bakarsın!",
+  name: "botekleme-şart",
+  description: "Botun istatistiğini görürsün!",
   type: 1,
   options: [],
 
   run: async(client, interaction) => {
-
+    const Uptime = moment
+    .duration(client.uptime)
+    .format(" D [gün], H [saat], m [dakika], s [saniye]");
     const embed = new EmbedBuilder()
-    .setTitle("Mercy Botlist - Yardım!")
     .setThumbnail("https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png")
-    .setDescription("\n**__Kısa Bilgi__**\n\n**<:r_mod:1033846559600807996> prefix:** */*\n<:r_tac:1034208114091769866> Sahib: <@847596022020309013>\n<:r_dunya:1034209747336957992> Ping: \`15\`\n\n**__Komutlar__**\n\n<:r_bot:1033846888681722007> \`Botlist\` Komutları İçin Tıkla\n<:r_mod:1033846559600807996> \`Moderasyon\` Komutları İçin Tıkla\n<:r_kullanici:1034207931178168412> \`Kullanıcı\` Komutları İçin Tıkla")
+    .setTitle("Bot Ekleme Şart!")
+    .setDescription("<:r_dikkat:1037060772284604536> | **Botunuz en az 1 hafta önce kurulmalıdır.** <:r_dikkat:1037060772284604536> | **Botunuz en az 10 sunucuda ekli olmalı.** **Botunuzda dm duyuru, özel mesaj gönderme, spam gibi komutlar olmamalıdır.**")
     .setColor("000000")
-    .setFooter({ text: "Mercy Botlist" })
-    const row = new Discord.ActionRowBuilder()
-interaction.reply({embeds: [embed], components: [row], ephemeral: true})
+    .setFooter({ text: "Bot Ekleme Şartları!" })
+    interaction.reply({embeds: [embed]})
+
   }
 
 };
