3fa1b00cd8e1982406c2b0a19be4987316932fce
Glitch (band-toothsome-broccoli) <none>
2022-12-31 14:42:54 +0000

    🎩👁 Checkpoint ./commands/ping.js:51438782/1021

--- commands/ping.js
+++ commands/ping.js
@@ -11,7 +11,10 @@ module.exports = {
     const { user, guildId, channel } = interaction;
 
 
-    interaction.reply({ embeds: [ new EmbedBuilder().setDescription(`• Gecikme değerleri anlık olarak gösterilir, zamanla bu değerler değişebilir.\nSoket Gecikmesi ${client.ws.ping}`).setAuthor({ name: `Gecikme değerleri | Mercy Botlist!`, iconURL: 'https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png?width=316&height=316' }).setColor("000000") ], ephemeral: true })
+    interaction.reply({ embeds: [ new EmbedBuilder()
+    .setAuthor({ name: `Moderasyon menüsü | Mercy Botlist!`, iconURL: 'https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png?width=316&height=316' })
+    .setDescription(`• Gecikme değerleri anlık olarak gösterilir, zamanla bu değerler değişebilir.\n\n**• Soket Gecikmesi:** \`143\`\n**• Veritabanı Gecikmesi:** \`${client.ws.ping}\`\n**• Veritabanı Durumu:** \`Bağlı\``)
+    .setColor("000000") ], ephemeral: true })
 
   }
 
