e8348566a838c6819cf85501e4c3f1615236042d
Glitch (band-toothsome-broccoli) <none>
2022-12-31 14:30:55 +0000

    🏃👍 Checkpoint ./commands/sil.js:51438782/40 ./commands/ping.js:51438782/536

--- commands/ping.js
+++ commands/ping.js
@@ -11,7 +11,7 @@ module.exports = {
     const { user, guildId, channel } = interaction;
 
 
-    interaction.reply({ embeds: [ new EmbedBuilder().setDescription(`<:Yesil:1033666717974548500> | \`${client.ws.ping}\` **Ms**`).setColor("000000") ], ephemeral: true })
+    interaction.reply({ embeds: [ new EmbedBuilder().setDescription(`• Gecikme değerleri anlık olarak gösterilir, zamanla bu değerler değişebilir.\nSoket Gecikmesi ${client.ws.ping}`).setAuthor({ name: `Gecikme değerleri | Mercy Botlist!`, iconURL: 'https://media.discordapp.net/attachments/1022843509016895568/1033978189078331392/f5e9db50825d34cd7bbe85a46091c791.png?width=316&height=316' }).setColor("000000") ], ephemeral: true })
 
   }
 
--- commands/sil.js
+++ commands/sil.js
@@ -17,7 +17,7 @@ module.exports = {
     if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) return interaction.reply({content: "**<:Kirmizi:1033666667181527062> | Bu komutu kullanabilmek için `Mesajları Yönet` yetkisine ihtiyacın var!**", ephemeral: true})
     const sayi = interaction.options.getString('sayı')
     interaction.channel.bulkDelete(sayi)
-    interaction.reply({content: "**<:Yesil:1033666717974548500> | Başarıyla belirtilen adet mesajı sildim.**"})
+    interaction.reply({content: "**<:Yesil:1033666717974548500> | Başarıyla `" + sayi + "` Adet Mesajı Sildim.**"})
 }
 
 };
