e9abb9ffac11a14825ef851027fbb808003d0c63
Glitch (band-toothsome-broccoli) <none>
2023-01-26 19:55:45 +0000

    ⛺️🚗 Checkpoint ./commands/destek.js:53396197/5063 ./commands/yardım.js:53396197/4840

--- commands/destek.js
+++ commands/destek.js
@@ -9,36 +9,36 @@ module.exports = {
   run: async(client, interaction) => {
 
     const embed = new EmbedBuilder()
-    .setAuthor({ name: `Yardım menüsü | Duster!`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
+    .setAuthor({ name: `Duster Bot Sunucuları`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
     .setThumbnail("https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414")
-    .setDescription("• Yardım almak için en doğru yerdesin, bir sorunun olursa destek sunucusunda seni bekliyor olacağız.\n\n**<:r_arti:1033844966360879245> | Yenilikler/Güncellemeler**\n• Botlist sistemini yeniledik ve sorunları giderdik. Artık daha kullanışlı. [Daha Fazla Bilgi.](https://discord.gg/ZyQWrgKg66)\n\n**<:r_ayarlar:1033687954419367936> | Komutlar [3]**\n**• Botlist -** Komutları İçin Tıkla!\n**• Moderasyon -** Komutları İçin Tıkla!\n**• Kullanıcı -**Komutları İçin Tıkla!\n\n**<:r_dikkat:1037060772284604536> | Bilgilendirme**\n• Katagoriler arasında geçiş yaparak daha fazla bilgiye ulaş.\n• Daha fazla botlist sunucusu istiyosan [Buraya Tıkla.](https://discord.gg/ZyQWrgKg66)")
+    .setDescription("•")
     .setColor("808080")
-    .setFooter({ text: `Duster • data#9999 Net`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
+    .setFooter({ text: `Duster • Mercy Botlist Sunucuları`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
     const row = new Discord.ActionRowBuilder()
     .addComponents(
 new Discord.ButtonBuilder()
 .setEmoji("<:971472504872915105:1068254460129849484>")
-.setLabel("Botlist")
+.setLabel("Mercy Code - Botlist 5k")
 .setStyle(Discord.ButtonStyle.Secondary)
-.setCustomId("kayıt"),
+.setURL("https://discord.gg/ZyQWrgKg66"),
 new Discord.ButtonBuilder()
 .setEmoji("<:971472504864510002:1068254488491724820>")
-.setLabel("Moderasyon")
+.setLabel("Mercy Botlist #1 | 1.7k")
 .setStyle(Discord.ButtonStyle.Secondary)
-.setCustomId("moderasyon"),
+.setURL("moderasyon"),
 new Discord.ButtonBuilder()
-.setEmoji("<:971472504923250768:1068254583715024996>")
-.setLabel("Kullanıcı")
+.setEmoji("<:r_kullanici:1034207931178168412>")
+.setLabel("Mercy Botlist #2 | 2.5k")
 .setStyle(Discord.ButtonStyle.Secondary)
-.setCustomId("Kullanıcı"),
+.setURL("Kullanıcı"),
 new Discord.ButtonBuilder()
 .setEmoji("<:971472504914849903:1068254517533098044>")
 .setLabel("Davet Et")
 .setStyle(Discord.ButtonStyle.Link)
-.setURL("https://discord.com/api/oauth2/authorize?client_id=1034130573796593714&permissions=8&scope=bot"),
+.setURL("Mercy Code - Altyapılar"),
 new Discord.ButtonBuilder()
-.setEmoji("<:r_discord:1034208049348485211>")
-.setLabel("Destek Sunucusu")
+.setEmoji("<:971472504558325801:1068254544624111686>")
+.setLabel("mryuptime.tk #7/24 Hizmet")
 .setStyle(Discord.ButtonStyle.Link)
 .setURL("https://discord.gg/ZyQWrgKg66"),)
 interaction.reply({embeds: [embed], components: [row],})
--- commands/yardım.js
+++ commands/yardım.js
@@ -9,36 +9,36 @@ module.exports = {
   run: async(client, interaction) => {
 
     const embed = new EmbedBuilder()
-    .setAuthor({ name: `Duster Bot Sunucuları`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
+    .setAuthor({ name: `Yardım menüsü | Duster!`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
     .setThumbnail("https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414")
-    .setDescription("•")
+    .setDescription("• Yardım almak için en doğru yerdesin, bir sorunun olursa destek sunucusunda seni bekliyor olacağız.\n\n**<:r_arti:1033844966360879245> | Yenilikler/Güncellemeler**\n• Botlist sistemini yeniledik ve sorunları giderdik. Artık daha kullanışlı. [Daha Fazla Bilgi.](https://discord.gg/ZyQWrgKg66)\n\n**<:r_ayarlar:1033687954419367936> | Komutlar [3]**\n**• Botlist -** Komutları İçin Tıkla!\n**• Moderasyon -** Komutları İçin Tıkla!\n**• Kullanıcı -**Komutları İçin Tıkla!\n\n**<:r_dikkat:1037060772284604536> | Bilgilendirme**\n• Katagoriler arasında geçiş yaparak daha fazla bilgiye ulaş.\n• Daha fazla botlist sunucusu istiyosan [Buraya Tıkla.](https://discord.gg/ZyQWrgKg66)")
     .setColor("808080")
-    .setFooter({ text: `Duster • Mercy Botlist Sunucuları`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
+    .setFooter({ text: `Duster • data#9999 Net`, iconURL: 'https://media.discordapp.net/attachments/1064266014671503380/1068234418201825320/wB4SYe8GAAhThFpK5m7Bow2Bp_lHE3CoWfMMcu2lrM4rK7Z6S0c3nUXTOKCtPlRKrCTQQCoHNaJEzBeiYK45rP3n.jpg?width=414&height=414' })
     const row = new Discord.ActionRowBuilder()
     .addComponents(
 new Discord.ButtonBuilder()
 .setEmoji("<:r_bot:1033846888681722007>")
-.setLabel("Mercy Code - Botlist 5k")
+.setLabel("Botlist")
 .setStyle(Discord.ButtonStyle.Secondary)
 .setCustomId("kayıt"),
 new Discord.ButtonBuilder()
 .setEmoji("<:r_mod:1033846559600807996>")
-.setLabel("Mercy Botlist #1 | 1.7k")
+.setLabel("Moderasyon")
 .setStyle(Discord.ButtonStyle.Secondary)
 .setCustomId("moderasyon"),
 new Discord.ButtonBuilder()
 .setEmoji("<:r_kullanici:1034207931178168412>")
-.setLabel("Mercy Botlist #2 | 2.5k")
+.setLabel("Kullanıcı")
 .setStyle(Discord.ButtonStyle.Secondary)
 .setCustomId("Kullanıcı"),
 new Discord.ButtonBuilder()
 .setEmoji("<:r_link:1034158510851772567>")
 .setLabel("Davet Et")
 .setStyle(Discord.ButtonStyle.Link)
-.setURL("Mercy Code - Altyapılar"),
+.setURL("https://discord.com/api/oauth2/authorize?client_id=1034130573796593714&permissions=8&scope=bot"),
 new Discord.ButtonBuilder()
 .setEmoji("<:r_discord:1034208049348485211>")
-.setLabel("mryuptime.tk #7/24 Hizmet")
+.setLabel("Destek Sunucusu")
 .setStyle(Discord.ButtonStyle.Link)
 .setURL("https://discord.gg/ZyQWrgKg66"),)
 interaction.reply({embeds: [embed], components: [row],})
